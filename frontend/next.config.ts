1a2982a014cd702cad26cd6c7089cd849fa9eb35
Amit-TheOne <amit.dew009@gmail.com>
2025-02-08 21:38:31 +0530

    config update

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config, { isServer }) {
+    // Workaround for canvas module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty',  // Fix for fs module missing in client-side
+        canvas: 'empty',  // Add canvas empty stub in client-side
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
